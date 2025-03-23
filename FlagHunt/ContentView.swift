a3a7d054cffc21c0a89aacb4be9040faa3929026
developerkamol <developerkamol@gmail.com>
2025-03-23 13:45:45 +0500

    Upgrades on design

--- FlagHunt/ContentView.swift
+++ FlagHunt/ContentView.swift
@@ -16,26 +16,55 @@ struct ContentView: View {
     @State private var scoreTitle = ""
     
     var body: some View {
-        ZStack() {
-            Color.indigo.ignoresSafeArea()
+        ZStack {
+            RadialGradient(stops: [
+                .init(color: Color(red: 0.05, green: 0.15, blue: 0.4), location: 0.3),
+                .init(color: Color(red: 0.9, green: 0.6, blue: 0.2), location: 0.3)
+            ], center: .top, startRadius: 200, endRadius: 700)
+                    .ignoresSafeArea()
             
-            VStack (spacing: 30) {
-                VStack {
-                    Text("Tap the flag of ")
-                        .foregroundStyle(.white)
-                    Text(countries[correctAnswer])
-                        .foregroundStyle(.white)
-                }
-                
-                ForEach(0..<3){ number in
-                    Button {
-                        flagTapped(number)
-                    } label: {
-                        Image(countries[number])
+            VStack{
+
+                Spacer()
+
+                Text("Hunt the flags!")
+                    .font(.largeTitle.weight(.bold))
+                    .foregroundStyle(.white)
+                VStack (spacing: 15) {
+                    VStack {
+                        Text("Tap the flag of ")
+                            .foregroundStyle(.secondary)
+                            .font(.subheadline.weight(.heavy))
+                        Text(countries[correctAnswer])                            .font(.largeTitle.weight(.semibold))
                     }
                     
+                    ForEach(0..<3){ number in
+                        Button {
+                            flagTapped(number)
+                        } label: {
+                            Image(countries[number])
+                                .clipShape(.capsule)
+                                .shadow(radius: 5)
+                        }
+                        
+                    }
                 }
+                .frame(maxWidth: .infinity)
+                .padding(.vertical, 20)
+                .background(.regularMaterial)
+                .clipShape(.rect(cornerRadius: 20))
+                
+                Spacer()
+                Spacer()
+                
+                Text("Score: ???")
+                    .foregroundStyle(.white)
+                    .font(.title.bold())
+                
+                Spacer()
+
             }
+            .padding()
         }
         .alert(scoreTitle, isPresented: $showingScore){
             Button("Continue", action: askQuestion)
