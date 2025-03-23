57c05753a7e213817588d71a31a02c4716260784
developerkamol <developerkamol@gmail.com>
2025-03-23 12:24:23 +0500

    Initial Commit

---
+++ FlagHunt/FlagHuntApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlagHuntApp.swift
+//  FlagHunt
+//
+//  Created by Kamol Madaminov on 23/03/25.
+//
+
+import SwiftUI
+
+@main
+struct FlagHuntApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
